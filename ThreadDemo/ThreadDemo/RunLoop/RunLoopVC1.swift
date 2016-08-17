ed4e532df7b07ea8ffc154c06ce66d04d02ab823
kang77649119 <77649119@qq.com>
2016-08-17 22:12:10 +0800

    广告轮播的优化 解决滑动广告的时候，定时器仍然运行的问题

--- ThreadDemo/ThreadDemo/RunLoop/RunLoopVC1.swift
+++ ThreadDemo/ThreadDemo/RunLoop/RunLoopVC1.swift
@@ -13,7 +13,7 @@ class RunLoopVC1: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITa
     let screenW = UIScreen.mainScreen().bounds.width
     let screenH = UIScreen.mainScreen().bounds.height
     
-    let pics = ["1.jpg" , "2.jpg" , "3.jpg"]
+    let pics = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"]
 
     var myTitle:String?
     
@@ -59,20 +59,20 @@ class RunLoopVC1: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITa
     
         let scrollView = UIScrollView(frame: CGRectMake(0, 64, screenW, 200))
         scrollView.backgroundColor = UIColor.blueColor()
-        scrollView.contentSize = CGSizeMake(screenW * 3, 0)
+        scrollView.contentSize = CGSizeMake(screenW * CGFloat(self.pics.count), 0)
         scrollView.pagingEnabled = true
         scrollView.bounces = true
         scrollView.showsVerticalScrollIndicator = false
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.delegate = self
         
-        let pageControlX = CGRectGetMaxX(scrollView.frame) - 50
+        let pageControlX = CGRectGetMaxX(scrollView.frame) - 80
         let pageControlY = CGRectGetMaxY(scrollView.frame) - 20
         let pageControlW:CGFloat = 50
         let pageControlH:CGFloat = 10
         
         let pageControl = UIPageControl(frame: CGRectMake( pageControlX, pageControlY, pageControlW, pageControlH))
-        pageControl.numberOfPages = 3
+        pageControl.numberOfPages = self.pics.count
         pageControl.currentPageIndicatorTintColor = UIColor.yellowColor()
         
         self.view.addSubview(scrollView)
@@ -84,7 +84,7 @@ class RunLoopVC1: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITa
         let imageW = scrollView.frame.size.width
         let imageH = scrollView.frame.size.height
         
-        for i in 0..<3 {
+        for i in 0..<self.pics.count {
             
             let imageX = scrollView.bounds.width * CGFloat(i)
             let imageView = UIImageView(frame: CGRectMake(imageX, 0, imageW, imageH))
@@ -93,6 +93,14 @@ class RunLoopVC1: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITa
             scrollView.addSubview(imageView)
             
         }
+        
+        // 开启定时器
+        startTimer()
+        
+    }
+    
+    // 开启定时器
+    func startTimer() {
         
         //        NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: #selector(self.adScroll), userInfo: nil, repeats: true)
         
@@ -103,6 +111,12 @@ class RunLoopVC1: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITa
         print(NSRunLoop.currentRunLoop())
         
     }
+    
+    // 关闭定时器
+    func stopTimer() {
+        self.timer?.invalidate()
+    }
+    
     
     /**
         初始化表格
@@ -133,8 +147,21 @@ class RunLoopVC1: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITa
         
     }
     
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        if scrollView.tag != 1001 {
+            self.stopTimer()
+        }
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.tag != 1001 {
+            self.startTimer()
+        }
+    }
+    
     func scrollViewDidScroll(scrollView: UIScrollView) {
         
+        // 表格的标识为1001，当非1001的控件滑动时，执行下面的方法
         if scrollView.tag != 1001 {
             let scrollX = scrollView.contentOffset.x / screenW
             let index = Int( scrollX )
@@ -152,15 +179,13 @@ class RunLoopVC1: UIViewController,UIScrollViewDelegate,UITableViewDelegate,UITa
      */
     func adScroll() {
         
-        if self.currentPage == 2 {
+        if self.currentPage == (self.pics.count-1) {
             self.currentPage = 0
             self.adScrollView?.contentOffset.x = 0
         } else {
-            
             self.currentPage += 1
             self.adPageControll?.currentPage = self.currentPage
             self.adScrollView?.contentOffset.x = screenW * CGFloat(self.currentPage)
-            
         }
         
     }
